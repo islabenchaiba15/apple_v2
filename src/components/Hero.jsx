88400d4b8565fbc82d971fab332ddf5a8da9e6e1
islambenchaiba <mi.benchaiba@esi-sba.dz>
2024-08-22 10:42:16 +0100

    mmm

---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useEffect } from "react";
+
+function Hero() {
+    const [videoSrc,setVideoSrc]=useState(window.innerWidth < 760 ? smallHeroVideo:heroVideo)
+    useGSAP(()=>{
+        gsap.to('#hero',{opacity:1,delay:2})
+        gsap.to('#col',{opacity:1,delay:2,y:-50})
+    },[])
+    const handleVideo =()=>{
+        if(window.innerWidth < 760 ){
+            setVideoSrc(smallHeroVideo)
+        }else{
+            setVideoSrc(heroVideo)
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener('resize', handleVideo)
+        return ()=>{
+            window.removeEventListener('resize', handleVideo)
+        }
+    },[])
+  return (
+    <div className="w-full h-[calc(100vh-60px)] bg-black relative">
+      <div className="w-full h-5/6 flex flex-col items-center justify-center">
+        <p id="hero"className="text-3xl opacity-0 text-center font-semibold text-gray-300 ">
+          Iphone 15 pro
+        </p>
+        <div className="w-9/12">
+          <video className={"pointer-events-none mt-10"}  muted autoPlay >
+            <source src={videoSrc} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+      <div id="col" className="flex flex-col items-center gap-4 opacity-0 mt-2">
+        <button className="btn ">Buy</button>
+        <p className="font-normal text-xl">from 199$/month or 999$</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { navLists } from '../constants'
+import { appleImg, bagImg, searchImg } from '../utils'
+
+function Navbar() {
+  return (
+    <div className='flex justify-between gap-10 items-center sm:px-10 px-5 py-5 w-full mw-auto relative max-w-[1200px]'>
+        <img src={appleImg} alt={'logo'} width={14} height={18} />
+        <div className="flex gap-8 items-center justify-between max-sm:hidden">
+            {navLists.map((nav,index)=>(
+                <div key={index} className="px-2 text-sm cursor-pointer text-gray-400 hover:text-white ">{nav}</div>
+            ))}
+        </div>
+        <div className="flex items-center justify-between gap-7">
+            <img src={searchImg} alt={'logo'} width={18} height={18} />
+            <img src={bagImg} alt={'logo'} width={14} height={18} />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
